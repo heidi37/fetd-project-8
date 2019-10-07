97ce54372511fa88da9d94413d20471f297e2ad9
Heidi <heidi37@users.noreply.github.com>
2019-10-06 22:24:14 -0400

    reordered the javaScript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var card = document.getElementsByClassName("card");
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on the card, open the modal
+// for ( let i = 0; i < card.length; i++) {
+//   card[i].onclick = function() {
+//     modal.style.display = "block";
+//   }
+// }
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 const container = document.getElementsByClassName("container")[1];
 
@@ -24,41 +58,6 @@ function generateCards(data) {
   } 
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var card = document.getElementsByClassName("card");
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-
-// When the user clicks on the card, open the modal
-// for ( let i = 0; i < card.length; i++) {
-//   card[i].onclick = function() {
-//     modal.style.display = "block";
-//   }
-// }
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 // ------------------------------------------
 //  HELPER FUNCTIONS

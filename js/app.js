cbeb57083988936303dc75740a494eabd3105da8
Heidi <heidi37@users.noreply.github.com>
2019-10-11 14:07:30 -0400

    final validation before submit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 const container = document.getElementsByClassName("container")[1];
-
-var card = document.getElementsByClassName("card");
-
 var cardIndex;
-
 var modal = document.getElementById("myModal");
-
 var span;
 
 
@@ -16,48 +11,47 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
     generateCards(data.results);
     generateModals(data.results);
   })
-  .catch(error => console.log('Looks like there was a problem', error))
+  .catch(error => console.log('Looks like there was a problem', error));
 
-  window.addEventListener('load', function () {
-    container.addEventListener('click', (e) => {
-      if (e.target.parentElement.parentElement.className === "card"
-      || e.target.parentElement.className === "card"
-      || e.target.className === "card") {
-        if(e.target.id !== '') {
-          cardIndex = e.target.id;
-          return cardIndex;
-        }
-        if(e.target.parentElement.id !== '') {
-          cardIndex = e.target.parentElement.id;
-          return cardIndex;
-        }
-        if(e.target.parentElement.parentElement.id !== '') {
-          cardIndex = e.target.parentElement.parentElement.id;
-          return cardIndex;
-        }
+window.addEventListener('load', function () {
+  container.addEventListener('click', (e) => {
+    if (e.target.parentElement.parentElement.className === "card"
+    || e.target.parentElement.className === "card"
+    || e.target.className === "card") {
+      if(e.target.id !== '') {
+        cardIndex = e.target.id;
+        return cardIndex;
       }
-    });
-  });
-
-  window.addEventListener('load', function () {
-    container.addEventListener('click', (e) => {
-      if (e.target.parentElement.parentElement.className === "card"
-      || e.target.parentElement.className === "card"
-      || e.target.className === "card") {
-            modal.style.display = "block";
-            modal.children[cardIndex].style.display= "block";
-            
-            //   Get the <span> element that closes the modal
-            span = modal.children[cardIndex].firstElementChild;
-
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-              modal.style.display = "none";
-              modal.children[cardIndex].style.display= "none";
-            };
+      if(e.target.parentElement.id !== '') {
+        cardIndex = e.target.parentElement.id;
+        return cardIndex;
       }
-    });
+      if(e.target.parentElement.parentElement.id !== '') {
+        cardIndex = e.target.parentElement.parentElement.id;
+        return cardIndex;
+      }
+    }
   });
+});
+
+window.addEventListener('load', function () {
+  container.addEventListener('click', (e) => {
+    if (e.target.parentElement.parentElement.className === "card" || e.target.parentElement.className === "card" || e.target.className === "card")
+    {
+      modal.style.display = "block";
+      modal.children[cardIndex].style.display= "block";
+      
+      //   Get the <span> element that closes the modal
+      span = modal.children[cardIndex].firstElementChild;
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+        modal.children[cardIndex].style.display= "none";
+      };
+    }
+  });
+});
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -82,12 +76,11 @@ function generateCards(data) {
     `;
     container.innerHTML += html;
   } 
-};
+}
 
 function generateModals(data) {
   for ( let i = 0; i < data.length; i++) {
     let html =
-
    `<div class="modal-content" id="m${[i]}">
     <span class="close">&times;</span>
     <img class="profile-image" src='${data[i].picture.large}' alt='${data[i].name.first} ${data[i].name.last}' />
@@ -103,13 +96,11 @@ function generateModals(data) {
   `;
   modal.innerHTML += html;
   }
-};
-
+}
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
-
 
 function checkStatus(response) {
   if(response.ok) {
